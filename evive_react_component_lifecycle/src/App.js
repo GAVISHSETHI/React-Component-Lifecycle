f2108d88aca81bf4124d6f38c14140bdb26627a6
gavish <gavish.sethi@evivehealth.com>
2017-10-23 18:17:20 +0530

    should update jsx

--- evive_react_component_lifecycle/src/App.js
+++ evive_react_component_lifecycle/src/App.js
@@ -1,57 +1,24 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
-import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import Wrapper from './update-no.jsx';
+import Shouldupdate from './shouldComponenUpdate.jsx';
 
-import 'semantic-ui-css/semantic.min.css';
+
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {val: 0}
-  }
-
-  update = () => {
-    this.setState({val: this.state.val + 1})
-  }
-  componentWillMount(){
-    console.log("component will mount");
-    this.setState({m: 2})
-  }
-
-  render() {
-    console.log('render');
-    return <Button color="red" onClick={this.update}>{this.state.val *  this.state.m}</Button>
-  }
-  componentDidMount() {
-    console.log("component did mount");
-      console.log(ReactDOM.findDOMNode(this));
-      this.inc = setInterval(this.update,5000)
-  }
-
-  componentWillUnmount(){
-    console.log('component will unmount');
-    clearInterval(this.inc)
-  }
-}
-
-class Wrapper extends Component {
-  mount = () => {
-  ReactDOM.render(<App />, document.getElementById('a'))
-  }
-
-  unmount = () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById('a'))
-  }
   render() {
     return (
-      <div>
-        <Button onClick={this.mount.bind(this)}>Mount</Button>
-        <Button onClick={this.unmount}>Unmount</Button>
-        <div id="a">
-
+      <div className="App">
+        <div>
+          <Router history={browserHistory}>
+            <Route path={"/"} component={Wrapper} />
+            <Route path={"should"} component={Shouldupdate} />
+          </Router>
         </div>
       </div>
-    )
+
+    );
   }
 }
-export default Wrapper;
+
+export default App;
---
+++ evive_react_component_lifecycle/src/home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+import Wrapper from './update-no.jsx';
+import { Button } from 'semantic-ui-react';
+import Shouldupdate from './shouldComponenUpdate.jsx';
+
+
+class Home extends Component {
+  render() {
+    return (
+
+
+    );
+  }
+}
+
+export default Home;
